2f8fca8ab0f3f970cc0316c830982df05dc56e54
hydrogenbondss <jeffreynicholas.t@gmail.com>
2026-09-20 20:30:26 +0800

    Expose Broccoli mark lab route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -56,6 +56,10 @@ export default function RootLayout() {
         name="(tabs)"
         options={{ headerShown: false }}
       />
+      <Stack.Screen
+        name="mark-lab"
+        options={{ headerShown: false }}
+      />
     </Stack>
   );
 }
